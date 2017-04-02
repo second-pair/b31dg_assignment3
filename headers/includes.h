350d6db3897edd2935886b74edbab4292bc5aa6c
Blair Edwards <blaired1@yahoo.co.uk>
2017-04-02 14:33:35 +0100

    Added serial comms Added extern parameters to 'includes.h' Added comment structure

--- headers/includes.h
+++ headers/includes.h
@@ -1,8 +1,56 @@
+/*  *--<Preface>--*  //
+
+ -=-  Author Details  -=-
+ Blair Edwards (BE38)
+ Heriot-Watt University
+
+ -=-  Part Of  -=-
+ b31dg_assignment3
+
+ -=-  Notes  -=-
+ =>  Data communication between threads uses the "Queue" method
+
+//  *--</Preface>--*  */
+
+
+
+//  Headers
 #include "mbed.h"
 #include "rtos.h"
 #include "MCP23017.h"
 #include "WattBob_TextLCD.h"
 
+//  Function Prototypes
+//  Since file-specific function headers would only have function prototypes
+//    in them, I'll just define them all here and save on multiple files.
 int main (void);
 int init (void);
 int initLcd (void);
+int initSerial (void);
+
+//  Logic Parameters
+#define TRUE 1
+#define FALSE 0
+#define HIGH 1
+#define LOW 0
+
+//  LCD Parameters
+extern MCP23017 *par_port;
+extern WattBob_TextLCD *lcd;
+
+//  Threads
+extern Thread recvDataThd;
+extern Thread sendDataThd;
+
+//  Data Queues
+extern Queue <char, 16> dataQueue;
+
+//  IO
+//  Serial Comms
+extern Serial pcSerial;
+//  Digital Inputs
+//extern DigitalIn aDigitalIn;
+//  Analogue Inputs
+//extern AnalogIn anAnalogueIn;
+//  Digital Outputs
+//extern DigitalOut aDigitalOut;
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,18 +1,74 @@
-/*
-https://docs.mbed.com/docs/mbed-os-api-reference/en/latest/APIs/tasks/rtos/
-https://www.element14.com/community/docs/DOC-46650/l/arm-keil-rtx-real-time-operating-system-overview
-*/
+/*  *--<Preface>--*  //
+
+ -=-  Author Details  -=-
+ Blair Edwards (BE38)
+ Heriot-Watt University
+
+ -=-  Dates  -=-
+ Started 2017-03-20
+ Edited  2017-04-02
+
+ -=-  Description  -=-
+ This programme attempts to create a Real-Time car controller.
+ This programme is intended to run on an MBED.
+ Input is through GPIO pins, the onboard ADC and hard-coded values.
+ Output is through GPIO pins, the onboard LCD and via serial port to a PC.
+ This programme is intended to talk to a real engine.  Since one might not
+    be available, an engine simulator has also been written.
+
+ -=-  Task  -=-
+ 1>  Read brake & accelerator values (use pots feeding to ADC)
+ 2>  Read engine on/off state from switch, display using onboard LED
+ 3>  Filter simulated speed (averaging last n readings)
+ 4>  Use RC Servo to simulate speedometer dial (averaged speed)
+ 5>  Have overspeed (> 70mph) LED (on red box)
+ 6>  Display on LCD:  Average speed, odometer (distance travelled)
+ 7>  Send to MAIL Queue:  Speed (from engine), acceleration (ADC), slowage (ADC)
+ 8>  Dump MAIL Queue to PC via Serial
+ 9>  Toggle sidelights with switch
+ 10> Turn indicator switches - if only one, flash 1KHz; if both, flash 2KHz
+
+ -=-  Notes  -=-
+ =>  Data communication between threads uses the "Queue" method
+
+ -=-  TODO  -=-
+ =>  Write many
+ =>  Make engine simulation toggleable?  Investigate
+
+//  *--</Preface>--*  */
+
+
+
+//  *--<Preparations>--*  //
 
 #include "includes.h"
 
+//  LCD Parameters
 MCP23017 *par_port;
 WattBob_TextLCD *lcd;
-//DigitalOut myled1 (LED1);
 
+//  Threads
 Thread recvDataThd;
 Thread sendDataThd;
+
+//  Data Queues
 Queue <char, 16> dataQueue;
 
+//  IO
+//  Serial Comms
+Serial pcSerial (USBTX, USBRX);
+//  Digital Inputs
+//DigitalIn aDigitalIn (p0);
+//  Analogue Inputs
+//AnalogIn anAnalogueIn (p0);
+//  Digital Outputs
+//DigitalOut aDigitalOut (p0);
+
+//  *--</Preparations>--*  //
+
+
+
+//  *--<Main Code>--*  //
 
 void sendData (void)
 {
@@ -47,11 +103,13 @@ int main (void)
     sendDataThd.signal_set (0x1);
 
     Thread::wait (osWaitForever);
+    return 0;
 }
 
 int init (void)
 {
     initLcd ();
+    initSerial ();
     return 0;
 }
 
@@ -67,3 +125,12 @@ int initLcd (void)
     lcd -> cls ();  //  Clear LCD
     return 0;
 }
+
+int initSerial (void)
+{
+    //  Send CSV Headers
+    pcSerial.printf("Blair Edwards Assignment 2\n");
+    pcSerial.printf ("Some Table Headers?\n");
+}
+
+//  *--</Main Code>--*  //
